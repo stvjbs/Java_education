3c7777615daed9748767069071b031fa54017bb7
stvjbs <yarashukartem@icloud.com>
2023-09-22 20:31:10 +0300

    sem4..

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,2 +1,11 @@
+import java.util.List;
+
 public class Example {
+    public class Example {
+        private List<? extends Number> obj;
+
+        public Example(List<? extends Number> obj) {
+            this.obj = obj;
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        public static void main(String[] args){
+            List<Double> o = new ArrayList<>();
+            List<?> h = new ArrayList<>();
+            Example ex = new Example(o);
+
+            Example ex1 = new Example(new ArrayList<>());
+        }
+    }
 }
